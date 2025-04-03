53fc69cb2eed25535ec35d17b3c397c1d417a629
ESP45182 <fegarcia@redhat.com>
2025-04-03 23:04:08 +0200

    DevSpave 2

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello SEAT from Barcelona!"
+		response = "Hello SEAT V2 from Barcelona!"
 	}
 
 	fmt.Fprintln(w, response)
