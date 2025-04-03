b01b3e9b120920636b995dbf0e89e81360f567b5
ESP45182 <fegarcia@redhat.com>
2025-04-03 21:21:16 +0000

    Prueba de commit en Git desde DevSpaces CLU

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello SEAT V2 from Barcelona!"
+		response = "Hello SEAT from DevSpaces!"
 	}
 
 	fmt.Fprintln(w, response)
